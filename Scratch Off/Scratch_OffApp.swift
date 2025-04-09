a5375e5f7a1aee75698425f588b7d6cc77f0c89d
Kiefer <kiefhay642@outlook.com>
2025-04-09 10:14:48 -0700

    Initial Commit

---
+++ Scratch Off/Scratch_OffApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scratch_OffApp.swift
+//  Scratch Off
+//
+//  Created by Kiefer Hay on 2025-04-09.
+//
+
+import SwiftUI
+
+@main
+struct Scratch_OffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Scratch OffTests/Scratch_OffTests.swift
@@ -0,0 +1,17 @@
+//
+//  Scratch_OffTests.swift
+//  Scratch OffTests
+//
+//  Created by Kiefer Hay on 2025-04-09.
+//
+
+import Testing
+@testable import Scratch_Off
+
+struct Scratch_OffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
